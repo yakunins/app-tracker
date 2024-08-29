94ce63454d3d820adca3bec74cf9f0f4c566a440
Sergey Yakunin <sergey@yakunins.com>
2024-08-29 14:48:41 -0700

    cleanup

--- install.cmd
+++ install.cmd
@@ -1,34 +1,27 @@
 @echo off
-
-:: getting startup folder (https://stackoverflow.com/a/68019702)
+:: getting startup folder of current user (https://stackoverflow.com/a/68019702)
 setlocal EnableExtensions DisableDelayedExpansion
-set "StartupFolder="
-for /F "skip=1 tokens=1,2*" %%I in ('%SystemRoot%\System32\reg.exe QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Startup 2^>nul') do if /I "%%I" == "Startup" if not "%%~K" == "" if "%%J" == "REG_SZ" (set "StartupFolder=%%~K") else if "%%J" == "REG_EXPAND_SZ" call set "StartupFolder=%%~K"
-if not defined StartupFolder for /F "skip=1 tokens=1,2*" %%I in ('%SystemRoot%\System32\reg.exe QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v Startup 2^>nul') do if /I "%%I" == "Startup" if not "%%~K" == "" if "%%J" == "REG_SZ" (set "StartupFolder=%%~K") else if "%%J" == "REG_EXPAND_SZ" call set "StartupFolder=%%~K"
-if not defined StartupFolder set "StartupFolder=\"
-if "%StartupFolder:~-1%" == "\" set "StartupFolder=%StartupFolder:~0,-1%"
-if not defined StartupFolder set "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-::endlocal
-::echo Startup folder of current user is: %StartupFolder%
+set "StartupDir="
+for /F "skip=1 tokens=1,2*" %%I in ('%SystemRoot%\System32\reg.exe QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Startup 2^>nul') do if /I "%%I" == "Startup" if not "%%~K" == "" if "%%J" == "REG_SZ" (set "StartupDir=%%~K") else if "%%J" == "REG_EXPAND_SZ" call set "StartupDir=%%~K"
+if not defined StartupDir for /F "skip=1 tokens=1,2*" %%I in ('%SystemRoot%\System32\reg.exe QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v Startup 2^>nul') do if /I "%%I" == "Startup" if not "%%~K" == "" if "%%J" == "REG_SZ" (set "StartupDir=%%~K") else if "%%J" == "REG_EXPAND_SZ" call set "StartupDir=%%~K"
+if not defined StartupDir set "StartupDir=\"
+if "%StartupDir:~-1%" == "\" set "StartupDir=%StartupDir:~0,-1%"
+if not defined StartupDir set "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+::echo %StartupDir%
 
 set CurrentDir=%CD%
 set ShortcutName=app-tracker-shortcut.lnk
 
-:: create shortcut with Powershell
+:: create shortcut with Powershell command
 set PWS_TARGET='%CurrentDir%\app-tracker.exe'
-set PWS_SHORTCUT='%CurrentDir%\%ShortcutName%'
+set PWS_SHORTCUT='%StartupDir%\%ShortcutName%'
 set PWS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
 %PWS% -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%PWS_SHORTCUT%); $S.TargetPath = %PWS_TARGET%; $S.Save()"
 
-:: move shortcut to startup
-set Location="%CurrentDir%\%ShortcutName%"
-set AutostartLocation="%StartupFolder%\%ShortcutName%"
-move %Location% %AutostartLocation%
-
-:PROMPT
-set /P OPENFOLDER=Open startup folder? Y/[N]
-if /I "%OPENFOLDER%" neq "Y" GOTO END
+:prompt
+set /P OPEN=Open startup folder? Y/[N]
+if /I "%OPEN%" neq "Y" goto end
 start shell:startup
-:END
+:end
 
 endlocal
